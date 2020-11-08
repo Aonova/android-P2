d0780d72ac24f74eb1d3133c27863237ff11a0ba
aonova <minar5islam@gmail.com>
2020-11-08 14:56:54 -0500

    Add workout button functionality

--- app/src/main/java/com/example/project2_tislam20/MainActivity.kt
+++ app/src/main/java/com/example/project2_tislam20/MainActivity.kt
@@ -7,11 +7,19 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         // "Edit Exercises" button
         findViewById<Button>(R.id.actMain_but_edit).setOnClickListener {
             val intent = Intent(this,ExercisesActivity::class.java)
+            startActivity(intent)
+        }
+
+        // "Workout" button
+        findViewById<Button>(R.id.actMain_but_start).setOnClickListener {
+            val intent = Intent(this,WorkoutActivity::class.java)
             startActivity(intent)
         }
     }
